d67939735f6d60349d1cffe9bd4a37f19e9f763e
Duron Epps <duroneppsjr@hotmail.com>
2017-03-29 20:59:04 +0000

    added picture upload to contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,8 @@ class Contact < ActiveRecord::Base
     validates :phone_number, presence: true
     validates :message, presence: true
     
+    attr_accessor :picture
+    
       has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   
